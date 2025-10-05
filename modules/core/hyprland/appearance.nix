c5296722843e1abd4a4a02ea814d9e1845d252b3
Matthew Mangano <matthew.mangano@gmail.com>
2025-10-04 17:03:20 -0700

    update hypr apper to max screen realestate

--- modules/core/hyprland/appearance.nix
+++ modules/core/hyprland/appearance.nix
@@ -4,7 +4,7 @@
   wayland.windowManager.hyprland.settings = {
     general = {
       gaps_in = 5;
-      gaps_out = 20;
+      gaps_out = 10;
       border_size = 2;
       "col.active_border" = "rgba(33ccffee) rgba(00ff99ee) 45deg";
       "col.inactive_border" = "rgba(595959aa)";
