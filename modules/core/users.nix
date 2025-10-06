568b91bfd7aefd70848c622f132377150a1581c7
Matthew Mangano <matthew.mangano@gmail.com>
2025-10-05 19:20:31 -0700

    added terminus iphone key

--- modules/core/users.nix
+++ modules/core/users.nix
@@ -8,6 +8,10 @@
     description = "Matthew";
     extraGroups = ["networkmanager" "wheel" "libvirtd"];
     shell = pkgs.zsh;
+    # Authorize SSH public keys declaratively (Terminust phone key)
+    openssh.authorizedKeys.keys = [
+      "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIM6PLgONXIR7zwtQziGNxQIjJWa/MlOIHm1U/E69/+FH terminus-phone"
+    ];
     packages = with pkgs; [];
   };
 
