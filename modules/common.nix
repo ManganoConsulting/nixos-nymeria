01707e67bd33a1c5536b64bfe882d55bc1ff6e65
Matthew Mangano <matthew.mangano@gmail.com>
2025-11-22 01:34:46 -0800

    use latest version of warp

--- modules/common.nix
+++ modules/common.nix
@@ -18,4 +18,20 @@
     ./core/git-repo-manager.nix
     ./core/screenshots.nix
   ];
+
+  # Override warp-terminal to a specific upstream build.
+  # TODO: replace url/hash with the .deb URL and sha256 for
+  #       v0.2025.11.19.08.12.stable_03 (see README instructions).
+  nixpkgs.overlays = [
+    (final: prev: {
+      warp-terminal = prev.warp-terminal.overrideAttrs (old: {
+        version = "0.2025.11.19.08.12.stable_03";
+
+        src = prev.fetchurl {
+          url = "<FILL_IN_WARP_DEB_URL_HERE>";
+          hash = "<FILL_IN_sha256_FROM_nix_store_prefetch-file>";
+        };
+      });
+    })
+  ];
 }
